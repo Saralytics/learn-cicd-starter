ea8a0ac63781e9c73fcc9eb43521b1c3805913ea
Saralytics <zhaoxue.li@outlook.com>
2024-07-01 20:24:08 +0400

    fix artifact name

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -4,4 +4,4 @@
 # CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o notely-amd64
 
 # Build for arm64
-CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o notely-arm64
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o notely
